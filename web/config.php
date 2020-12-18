8652f2c7ebabf6f45a943ef68374ad95726358a8
dora-admin001 <67258691+dora-admin001@users.noreply.github.com>
2020-12-18 16:18:22 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -2,7 +2,7 @@
 $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyBi-TM9dGLHa0p0dxm8KY6_Ry5jbZAzCiE');
-define('GJ_CODE', 'MY');
+define('GJ_CODE', 'US');
 define('SITE_NAME', '神秘国度');
 define('TITLENAME', '神秘国度');
 define('EN2DEKEY', 'AnQUP1XDEF87ewemhuYG35e67CtzDSh5');
